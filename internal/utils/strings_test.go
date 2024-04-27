967d0b5a2ec5de13e0b3f2c9c3f6384e450d5d0a
ossan <ivan.pesenti.dev@gmail.com>
2024-04-27 12:12:24 +0200

    fuzz tests

--- internal/utils/strings_test.go
+++ internal/utils/strings_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/ossan-dev/gotesting/internal/utils"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestReverseString(t *testing.T) {
@@ -16,4 +17,16 @@ func TestReverseString(t *testing.T) {
 	if actual != expected {
 		t.Errorf("want: %q, got %q", expected, actual)
 	}
+}
+
+func FuzzReverseString(f *testing.F) {
+	testCases := []string{"Ninja", "Hello, World!", ""}
+	for _, tc := range testCases {
+		f.Add(tc) // add tests to seed corpus
+	}
+	f.Fuzz(func(t *testing.T, input string) { // fuzz target func
+		reversed := utils.ReverseString(input)
+		reversedTwice := utils.ReverseString(reversed)
+		assert.Equal(t, input, reversedTwice)
+	})
 }
