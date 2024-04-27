0bdba7432c35025b852cec8d5bd6e3edfb892d64
ossan <ivan.pesenti.dev@gmail.com>
2024-04-27 16:04:49 +0200

    introduced 'testify/suite'

--- internal/user/user_test.go
+++ internal/user/user_test.go
@@ -8,24 +8,42 @@ import (
 	"github.com/ossan-dev/gotesting/internal/user"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 // BEFORE: docker run -d -p 54322:5432 -e POSTGRES_PASSWORD=postgres postgres
-func TestAddUser(t *testing.T) {
+type userSuite struct {
+	suite.Suite
+	gormClient *gorm.DB
+}
+
+func TestUserSuite(t *testing.T) {
+	suite.Run(t, new(userSuite))
+}
+
+func (s *userSuite) SetupSuite() {
 	// Arrange
-	db, err := gorm.Open(postgres.Open("host=localhost port=54322 user=postgres password=postgres dbname=postgres sslmode=disable"))
-	require.NoError(t, err)
-	err = db.AutoMigrate(&user.User{})
-	require.NoError(t, err)
+	var err error
+	s.gormClient, err = gorm.Open(postgres.Open("host=localhost port=54322 user=postgres password=postgres dbname=postgres sslmode=disable"))
+	require.NoError(s.T(), err)
+	err = s.gormClient.AutoMigrate(&user.User{})
+	require.NoError(s.T(), err)
+}
+
+func (s *userSuite) SetupTest() {
+	require.NoError(s.T(), s.gormClient.Delete(&user.User{}, "1 = 1").Error)
+}
+
+func (s *userSuite) TestAddUser() {
 	// Act
-	err = user.AddUser(db, &user.User{Name: "john doe"})
+	err := user.AddUser(s.gormClient, &user.User{Name: "john doe"})
 	// Assert
-	require.NoError(t, err)
+	require.NoError(s.T(), err)
 	var userInDB user.User
-	err = db.Where("Name = ?", "john doe").First(&userInDB).Error
-	assert.NoError(t, err)
+	err = s.gormClient.Where("Name = ?", "john doe").First(&userInDB).Error
+	assert.NoError(s.T(), err)
 }
 
 // go test -tags="integration" -v
